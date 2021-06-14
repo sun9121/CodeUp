b51c6e12c27999760b65d8802e214dd896374741
Moonsun <choims9121@gmail.com>
2021-06-14 15:21:50 +0900

    210614_2

---
+++ Python_basic/64.py
@@ -0,0 +1,11 @@
+#  입력된 세 정수 a, b, c 중 가장 작은 값을 출력하는 프로그램을 작성해보자.
+# 단, 3항 연산을 사용한다.
+
+# a,b,c = map(int, input().split())
+# list = [a,b,c]
+# print(min(list))
+
+# 이렇게 걍 하고싶네
+# 이건 3항연산을 사용하지 않은 방법 ㅠㅜ
+
+a,b,c = map(int, input().split())
