02f3ea8eadef7cc9bc1cb3b3b4cdb8ee156ecb70
Moonsun <choims9121@gmail.com>
2021-06-14 12:46:51 +0900

    210614_1

---
+++ Python_basic/17.py
@@ -0,0 +1,9 @@
+# 정수(integer), 실수, 문자(character), 문자열(string) 등 1개만 입력받아 한 줄로 3번 출력해보자.
+
+# 예시
+# s = input()
+# print(s, s, s)  #공백으로 구분해 한 줄로 출력한다.
+# 와 같은 방법으로 3번 출력할 수 있다.
+
+s = input()
+print(s, s, s)
