bea77678de821353296efc03f5c4e07dd798f42e
Moonsun <choims9121@gmail.com>
2021-06-28 17:02:14 +0900

    not easy

--- CoTe/07_Find part 03.py
+++ CoTe/07_Find part 03.py
@@ -9,25 +9,20 @@ m = int(input())
 list_cstm = list(map(int, input().split()))
 
 def binary_search(target, array, start, end):
-    if start > end:
-        # print('start : ', start)
-        return None
-    mid = (start + end) // 2
-    # print(mid)
-    if array[mid] == target:
-        # print(f'mid:{mid}')
-        # print(target)
-        # print(array[mid])
-        return mid + 1
-    elif array[mid] < target:
-        return binary_search(target, array, mid+1, end)
-    else:
-        return binary_search(target, array, start, mid-1)
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid + 1
+        elif array[mid] < target:
+            return binary_search(target, array, mid+1, end)
+        else:
+            return binary_search(target, array, start, mid-1)
+
+    return None
 
 for i in list_cstm:
-    # print(f'i:{i}')
     result = binary_search(i, list_shop, 0, n-1)
-    # print(result)
+
     if result != None:
         print('yes', end=' ')
     else:
