c0dde6b361f58082f285c6b4eb56eab25e1d5a82
Moonsun <choims9121@gmail.com>
2021-07-05 17:22:09 +0900

    greedy_1

--- CoTe/Part02/10_City divide plan.py
+++ CoTe/Part02/10_City divide plan.py
@@ -1,17 +1,10 @@
-v, e = map(int, input().split())
+n, m = map(int, input().split())
 
-parent = [0] * (v+1)
+edges = []
+parent = [0]*(n+1)
 
-lines = []
-result = 0
-
-for _ in range(e):
-    a, b, cost = map(int, input().split())
-
-    lines.append((cost,a,b))
-
-lines.sort()
-last = 0
+for i in range(n+1):
+    parent[i] = i
 
 def find_parent(parent, x):
     if parent[x] != x:
@@ -22,17 +15,25 @@ def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
     if a < b:
-        parent[b]=a
+        parent[b] = a
     else:
-        parent[a]=b
+        parent[a] = b
 
+for _ in range(m):
+    cost, a, b = map(int, input().split())
 
-for line in lines:
-    cost, a, b = line
+    edges.append((cost, a, b))
+
+edges.sorts()
+cost_sum = 0
+last = 0 # 신장트리를 두 부분으로 나누기 위해서 가장 큰 부분을 삭제해준다. 그럼 최소 조건에 맞게끔 나뉘겠지.
+
+for edge in edges:
+    cost, a, b = edge
 
     if find_parent(parent, a) != find_parent(parent, b):
         union_parent(parent, a, b)
-        result += cost
+        cost_sum += cost
         last = cost
 
-print(result - last)
+print(cost_sum - last)
--- CoTe/Part02/10_Team organization.py
+++ CoTe/Part02/10_Team organization.py
@@ -1,8 +1,10 @@
 n, m = map(int, input().split())
 
-parent = [0]* (n+1)
+# team = [[]*(m)]
 
-for i in range(0,n+1):
+parent = [0]*(n+1)
+
+for i in range(n+1):
     parent[i] = i
 
 def find_parent(parent, x):
@@ -10,23 +12,39 @@ def find_parent(parent, x):
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
-
 def union_parent(parent,a,b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
-    if a<b:
-        parent[b] = a
 
+    if a < b:
+        parent[b] = a
     else:
         parent[a] = b
 
-for i in range(m):
-    oper, a, b = map(int, input().split())
+result = []
+count = 0
+for _ in range(m):
+    cal, a, b = map(int, input().split())
+    
+    if cal == 0:
+        union_parent(parent, a, b)
 
-    if oper == 0:
-        union_parent(parent,a,b)
-    elif oper == 1:
+    elif cal == 1:
+        count+=1
         if find_parent(parent,a) == find_parent(parent, b):
-            print('YES')
+            result.append('YES')
         else:
-            print('No')
+            result.append('NO')
+
+for i in range(count):
+    print(result[i], end = '\n')
+
+# for i in team:
+#     if i[0] == 0:
+#         union_parent(parent, i[1], i[2])
+
+#     elif i[0] == 1:
+#         if find_parent(parent,a) == find_parent(parent, b):
+#             print('YES')
+#         else:
+#             print('NO')
